3629a43f9be410c9ee844ecb4158150116daa7ff
shimomo <developer@shimomo.net>
2025-04-08 00:57:04 +0900

    test: Merge `ConverterTest.php` and `ConverterExceptionTest.php` to remove duplication

--- tests/ConverterTest.php
+++ tests/ConverterTest.php
@@ -430,4 +430,45 @@ final class ConverterTest extends TestCase
     {
         $this->assertSame($expected, Converter::convertToWindDirectionName(...$arguments));
     }
+
+    /**
+     * @return void
+     */
+    public function testInvalidTooFewArguments(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage(
+            'Too few arguments to function BVP\Converter\ConverterCore::invalid(), ' .
+            '0 passed and exactly 1 expected.'
+        );
+
+        Converter::invalid();
+    }
+
+    /**
+     * @return void
+     */
+    public function testInvalidTooManyArguments(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage(
+            'Too many arguments to function BVP\Converter\ConverterCore::invalid(), ' .
+            '2 passed and exactly 1 expected.'
+        );
+
+        Converter::invalid(1, 2);
+    }
+
+    /**
+     * @return void
+     */
+    public function testInvalidUndefinedMethod(): void
+    {
+        $this->expectException(\BadMethodCallException::class);
+        $this->expectExceptionMessage(
+            'Call to undefined method BVP\Converter\Converters\CoreConverter::invalid().'
+        );
+
+        Converter::invalid(1);
+    }
 }
